8f89414156a01d82c3dccdeb2933d1af41ea6f17
Martí <marti@carasus.(none)>
2014-09-13 13:57:35 +0200

    Node de control de tf i publisher de color. configuracio rviz. tot afegit a launch file

--- live_demo/src/arduino_demo/arduino_demo.ino
+++ live_demo/src/arduino_demo/arduino_demo.ino
@@ -1,17 +1,19 @@
 #include <ros.h>
 #include <Servo.h> 
-#include <std_msgs/Float32.h>
+#include <std_msgs/Int32.h>
 #include <std_msgs/ColorRGBA.h>
 
 #define PIN_LED_VERMELL 3
 #define PIN_LED_VERD 5
 #define PIN_LED_BLAU 6
-#define PIN_MOTOR ??
-#define PIN_POTENCIOMETRE ??
+#define PIN_MOTOR 9
+#define PIN_POTENCIOMETRE A0
 
 ros::NodeHandle nh;
 
 // === publisher ===
+std_msgs::Int32 pot_value;
+ros::Publisher potenciometrePub("/potenciometre", &pot_value);
 std_msgs::Int32 motor_value;
 ros::Publisher motorPub("/posicio", &motor_value);
 Servo motor_servo;
@@ -24,27 +26,32 @@ void colorCb( const std_msgs::ColorRGBA& color_value){
   analogWrite( PIN_LED_VERD,    color_value.g );
   analogWrite( PIN_LED_BLAU,    color_value.b );
 }
-ros::Subscriber<::> colorSub("color", &colorCb );
+ros::Subscriber<std_msgs::ColorRGBA> colorSub("color", &colorCb );
 
 // === estructura Arduino ===
 void setup() {
   nh.initNode();
+  nh.advertise(potenciometrePub);
   nh.advertise(motorPub);
   nh.subscribe(colorSub);
- 
+  std_msgs::ColorRGBA c;
+  colorCb(c);
+
   motor_servo.attach(PIN_MOTOR);
   motor_servo.write(90);  // set servo to mid-point
 }
 void loop() {
   // ========== MOTOR ===========
   // llegir estat del boto
-  int posicio = analogRead(PIN_POTENCIOMETRE)*(5.0/255.0);
+  int posicio = int(analogRead(PIN_POTENCIOMETRE)*(255.0/1023.0));  
   posicio = max(posicio, 0);
   posicio = min(posicio, 255);
+  pot_value.data = posicio;
+  potenciometrePub.publish( &pot_value );
   // enviar el motor un pas mes enlla
   motor_servo.write(posicio);
   // enviar dades de posicio del motor pel topic
-  motor_value.data  = servo.read();
+  motor_value.data  = motor_servo.read();
   motorPub.publish( &motor_value );
 
   nh.spinOnce();
---
+++ live_demo/src/node_demo.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import rospy
+import tf
+from std_msgs.msg import Int32
+from std_msgs.msg import ColorRGBA
+import math
+
+def posicioCb(msg):
+    br = tf.TransformBroadcaster()
+    br.sendTransform((0.007, 0.0, 0.0125), tf.transformations.quaternion_from_euler
+              (0, 0, math.radians(msg.data)), rospy.Time.now(), "helice_eix", "cos")    
+    c = ColorRGBA()
+    #wolframalpha: graph sin (2*t),sin (2*t+pi/3*2),sin (2*t+pi*2/3*2) from t=0 to t=pi
+    c.r=max(255*(math.sin(math.radians(msg.data)*2.0)),0)
+    c.g=max(255*(math.sin(math.radians(msg.data)*2.0+2.0/3.0*math.pi)),0)
+    c.b=max(255*(math.sin(math.radians(msg.data)*2.0+4.0/3.0*math.pi)),0)
+    pub.publish(c)
+
+if __name__ == '__main__':
+    rospy.init_node('node_demo')
+    print "node_demo INICIAT "
+    rospy.Subscriber('/posicio', Int32, posicioCb)
+    pub = rospy.Publisher('/color', ColorRGBA)
+    rospy.spin()
